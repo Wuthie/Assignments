90be15502d5303873aeedf84fa83943a4dedefa8
Wuthie <panhwuthie@gmail.com>
2022-10-25 13:49:13 -0400

    new file submission

--- A3_Q1.java
+++ A3_Q1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // -------------------------------------------------------
 // Assignment A3_Q1
 // Written by: Wuthie PANHA 40245548
@@ -6,26 +8,22 @@
 
 // program take in plain text and shows the encrypted version to the user
 
-import java.util.Scanner;
-import java.util.concurrent.CountDownLatch;
 public class A3_Q1 {
-
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Welcome to the 3D-Space Encryption-Decryption Program:" + 
                          "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++" + 
                          "\n\nPlease enter your plain text below: ");
-        char[] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
         String plainText = keyboard.nextLine();
         keyboard.close();
-        System.out.print(plainText.charAt(2));
-        int count = 0;
-        String encryptedText[] = {} ;
-        for(count =0; count == plainText.length(); count += 1){
-            char character = plainText.charAt(count);
+        int textLength = plainText.length();
+        for(int i = 0; i < textLength; i+=1){
+            char character = plainText.charAt(i);
             if (Character.isLetter(character)){
-                char i = (char)((character + 1)%26);
+
             }
+
         }
+
     }
 }
---
+++ test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        char [] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    
+        System.out.println(letters);
+    }
+}
